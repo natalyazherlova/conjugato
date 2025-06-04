218276785799a5799b2e260e61ac421461f3dd00
Natasha <zherlova@gmail.com>
2025-06-04 12:09:00 +0200

    Initial Commit

---
+++ ConjuGatoTestProject/ConjuGatoTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConjuGatoTestProjectApp.swift
+//  ConjuGatoTestProject
+//
+//  Created by Наталья Жерлова on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ConjuGatoTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConjuGatoTestProjectTests/ConjuGatoTestProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConjuGatoTestProjectTests.swift
+//  ConjuGatoTestProjectTests
+//
+//  Created by Наталья Жерлова on 04/06/2025.
+//
+
+import Testing
+@testable import ConjuGatoTestProject
+
+struct ConjuGatoTestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
