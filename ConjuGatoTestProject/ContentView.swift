cd2989bcc45fc9fbd99ef3c61bf3728d225ebd9b
Natasha <zherlova@gmail.com>
2025-06-04 12:13:29 +0200

    first commit

--- ConjuGatoTestProject/ContentView.swift
+++ ConjuGatoTestProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("It's going to be a lookalike to ConjuGato app")
         }
         .padding()
     }
